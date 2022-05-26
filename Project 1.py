cfe8a7a8b5eca053d87ad4b3905ebbdf3591602c
Nurlan Turganbay <38539412+CaDucT@users.noreply.github.com>
2022-05-26 20:44:19 +0200

    Update Project 1.py

--- Project 1.py
+++ Project 1.py
@@ -1,14 +1,3 @@
-"""
-Implement a program that will launch a specified process and periodically (with a provided time interval) collect the
-following data about it:
-    • CPU usage (percent);
-    • Memory consumption: Working Set and Private Bytes (for Windows systems) or Resident Set Size and Virtual Memory Size (for Linux systems);
-    • Number of open handles (for Windows systems) or file descriptors (for Linux systems).
-Data collection should be performed all the time the process is running. Path to the executable file for the process
-and time interval between data collection iterations should be provided by user. Collected data should be stored on the
-disk. Format of stored data should support automated parsing to potentially allow, for example, drawing of charts.
-"""
-
 import psutil
 import csv
 import os
